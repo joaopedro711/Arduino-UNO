2b4e8c19d3f672e378d9e855e3aa6da230343649
joaopedro711 <joaopedro711@github.com>
2021-12-02 16:45:50 -0300

    uno

--- Abastecimento_de_agua.cpp
+++ Abastecimento_de_agua.cpp
@@ -1,54 +1,67 @@
-//Sistema de abastecimento de reservatorio de agua
+#include <LiquidCrystal.h> // Inclusão da biblioteca do LCD
+LiquidCrystal lcd (2, 3, 4, 5, 6, 7); // define os pinos de envio de dados do LCD
 
-int ledverde = 11;
-int ledvermelho = 12;
-int motor = 13;
-int sensorbaixo = 6;
-int sensoralto = 7;
+#define Bomba1_LED 12
+#define Bomba2_LED 11
+#define Bomba3_LED 10
+#define Alarme_Max 8
+#define Alarme_Min 9
+#define POT A2
 
+void setup() {
 
-int a, b;
+  lcd.begin(16, 2); // declara que o LCD contém 16 linhas e 2 colunas
+  lcd.setCursor(0, 0); // declara que o cursor do LCD estará na linha 0 e coluna 0
+  lcd.print("Volume em Litros"); // Imprime na tela do LCD a informação "Volume em Litros"
 
-void setup(){
-    pinMode(ledverde, OUTPUT);
-    pinMode(ledvermelho, OUTPUT);
-    pinMode(motor, OUTPUT);
-    pinMode(sensoralto, INPUT);
-    pinMode(sensorbaixo, INPUT);
-    digitalWrite(sensorbaixo, HIGH);
-    digitalWrite(sensoralto, HIGH);
+  pinMode(Bomba1_LED, OUTPUT); // declara que a variável Bomba1_LED é uma saída
+  pinMode(Bomba2_LED, OUTPUT); // declara que a variável Bomba2_LED é uma saída
+  pinMode(Bomba3_LED, OUTPUT); // declara que a variável Bomba3_LED é uma saída
+  pinMode(Alarme_Max, OUTPUT); // declara que a variável Alarme_Max é uma saída
+  pinMode(Alarme_Min, OUTPUT); // declara que a variável Alarme_Min é uma saída
+  pinMode(POT, INPUT); // declara que a variável POT é uma entrada
 }
 
-void loop(){
-    a = digitalRead(sensorbaixo);   //0 == se esta enchendo, 1 == vazia
-    b = digitalRead(sensoralto);    //0 == nivel apice da caixa
+void loop() {
 
-    //se a caixa estiver vazia
-    if((a == LOW || a == HIGH) && b == LOW)
-        ligamotor();
+  int nivel = analogRead (POT); // Declara a variável nivel como sendo a do potenciômetro
 
-    //se a caixa estiver cheia    
-    else if(a == HIGH && b == HIGH)
-        desligamotor();
+  if (nivel >= 1) { // Se o nível do reservatório for >= 1 imprime no LCD a variável nível (valor do potenciômetro)
+    lcd.setCursor(7, 16);
+    lcd.print(nivel);
+    lcd.print("               "); //imprime espaço em branco para limpar a coluna 2
+  }
 
-    //caso os sensores deem diferentes    
-    else if(a == LOW && b == HIGH)
-        erro();
+  if ((nivel <= 200) && (nivel >= 1)) { // Se o nível do reservatório for <= a 200 "E" >= a 1, todas as bombas ligam
+    digitalWrite (Bomba1_LED, HIGH);
+    digitalWrite (Bomba2_LED, HIGH);
+    digitalWrite (Bomba3_LED, HIGH);
+  }
+  else if (nivel >= 201) { // Se não o nível do reservatório for >= a 201, a Bomba 1 e 3 desligam
+    digitalWrite (Bomba1_LED, LOW);
+    digitalWrite (Bomba3_LED, LOW);
+  }
 
-}
+  if ((nivel <= 150) && (nivel >= 1)) { // Se o nível do reservatório for <= a 150 "E" >= 1 o alarme mínimo aciona
+    tone (Alarme_Min, 440); // toca alarme em 440 Hz
+  }
 
-void ligamotor(){
-    digitalWrite(ledverde, HIGH);
-    digitalWrite(ledvermelho, LOW);
-    digitalWrite(motor, HIGH);
-}
-void desligamotor(){
-    digitalWrite(ledverde, LOW);
-    digitalWrite(ledvermelho, HIGH);
-    digitalWrite(motor, LOW);
-}
-void erro(){
-    digitalWrite(ledverde, HIGH);
-    digitalWrite(ledvermelho, HIGH);
-    digitalWrite(motor, LOW);
+  else if (nivel >= 200) { // Se não o nível do reservatório for >= a 200 o alarme mínimo desliga
+    noTone (Alarme_Min); // desliga alarme mínimo
+  }
+
+  if (nivel == 900) { // Se o nível do reservatório for == a 900, a bomba 2 desliga
+    digitalWrite (Bomba2_LED, LOW);
+  }
+
+  if (nivel >= 950) { // Se o nível do reservatório for >= 950 o alarme máximo aciona
+    tone (Alarme_Max, 440); // toca alarme em 440 Hz
+    lcd.setCursor(0, 0);
+
+    lcd.print("Reservatorio Cheio");
+  }
+
+  else if (nivel < 950) { // Se não o nível do reservatório for < que 950 o alarme máximo desliga
+    noTone (Alarme_Max); // desliga alarme máximo
+  }
 }
